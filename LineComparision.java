e567c3b58675fa62b1eac9820222a996ee86a7f9
saurabh <mrsaurabhshinde5@gmail.com>
2023-02-04 18:35:12 +0530

    Initial Commit

---
+++ LineComparision.java
@@ -0,0 +1,14 @@
+package com.bl.Day3;
+
+import java.util.Scanner;
+
+public class LineComparision
+{
+    public static void main(String[] args) {
+        System.out.println("welcome to line comparison computation program on master branch");
+
+
+
+    }
+
+}
